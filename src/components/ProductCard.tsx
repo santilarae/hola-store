207da953f8262243c3fb9bcaf3dc5501c2f946bd
SantiagoLaraE <oscarescobar773@gmail.com>
2023-08-08 09:45:54 -0500

    Different Product card for shopping cart

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,16 +1,32 @@
 import { Link } from 'react-router-dom'
-import { CartIcon } from './Icons'
-import { IProduct } from '../types/products'
-import { MouseEventHandler, useEffect, useRef } from 'react'
+import { CartIcon, CloseIcon } from './Icons'
+import { IProduct, IProductCart } from '../types/products'
+import { ChangeEvent, MouseEventHandler, useEffect, useRef } from 'react'
 import { useAppDispatch, useAppSelector } from '../hooks/redux'
-import { addToCart } from '../store/slices/cart'
+import {
+  addToCart,
+  removeFromCart,
+  updateProductQty
+} from '../store/slices/cart'
 import { UIComponents, openComponent } from '../store/slices/ui'
 
-const ProductCard = ({ product }: { product: IProduct }) => {
-  const imgRef = useRef<HTMLImageElement>(null)
+interface ProductCardProps {
+  product: IProductCart | IProduct
+  inCart?: boolean
+}
+
+const ProductCard = ({ product, inCart }: ProductCardProps) => {
+  return inCart ? (
+    <ProductCardInCart product={product as IProductCart} />
+  ) : (
+    <ProductCardCommon product={product} />
+  )
+}
+
+const ProductCardCommon = ({ product }: { product: IProduct }) => {
   const { products } = useAppSelector(state => state.cart)
   const dispatch = useAppDispatch()
-  
+
   const isInCart =
     products.findIndex(p => p.id === product.id) >= 0 ? true : false
 
@@ -22,7 +38,7 @@ const ProductCard = ({ product }: { product: IProduct }) => {
       dispatch(addToCart(product))
     }
   }
-
+  const imgRef = useRef<HTMLImageElement>(null)
   useEffect(() => {
     const intersectionObserver = new IntersectionObserver(
       (entries, observer) => {
@@ -75,6 +91,63 @@ const ProductCard = ({ product }: { product: IProduct }) => {
         </div>
       </article>
     </Link>
+  )
+}
+
+const ProductCardInCart = ({ product }: { product: IProductCart }) => {
+  const dispatch = useAppDispatch()
+  const qtyArr = Array(10).fill('')
+
+  const handleQty = (e: ChangeEvent<HTMLSelectElement>, id: number) => {
+    dispatch(
+      updateProductQty({
+        id,
+        qty: Number(e.target.value)
+      })
+    )
+  }
+
+  const handleRemoveProduct = (id: number) => {
+    dispatch(removeFromCart(id))
+  }
+  return (
+    <div
+      key={`product-cart${product.id}`}
+      className='border-b border-neutral grid grid-cols-3 p-4 gap-2 items-center'
+    >
+      <img
+        src={product.images[0]}
+        alt={product.title}
+        className='aspect-square shrink-0 w-full rounded'
+        width={640}
+        height={640}
+      />
+      <div className='col-span-2'>
+        <p className='flex font-bold items-start justify-between'>
+          <span className='pt-1'>{product.title}</span>
+          <button
+            className='shrink-0 p-2'
+            onClick={() => handleRemoveProduct(product.id)}
+          >
+            <CloseIcon className='w-4 h-4 grayscale contrast-0 opacity-50 hover:opacity-100 transition-opacity' />
+          </button>
+        </p>
+        <p className='truncate text-sm'>{product.description}</p>
+        <p>
+          Quantity:
+          <select name='quantity' onChange={e => handleQty(e, product.id)}>
+            {qtyArr.map((_, i) => (
+              <option key={`cart-qty=${i + 1}`} value={i + 1}>
+                {i + 1} {i + 1 >= 10 ? 'units' : 'unit'}
+              </option>
+            ))}
+          </select>
+        </p>
+        <p>
+          {product.quantity} x ${product.quantity * product.price}
+        </p>
+      </div>
+    </div>
   )
 }
 
